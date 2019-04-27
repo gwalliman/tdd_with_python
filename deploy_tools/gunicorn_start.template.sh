b6ad7dffae7c1a0d7b934ba91858f4416e49826d
Garret Walliman <garretwalliman@gmail.com>
2019-04-26 23:01:20 -0700

    Updating templates

--- deploy_tools/gunicorn_start.template.sh
+++ deploy_tools/gunicorn_start.template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd <PATH/TO/DJANGO/ROOT>
-. <PATH/TO/DJANGO/ROOT>/bin/activate
+cd /home/ubuntu/sites/DOMAIN
+. /home/ubuntu/sites/DOMAIN/bin/activate
 python3.6 manage.py collectstatic --noinput
-gunicorn settings.config.wsgi:application -b unix:/tmp/<SOCKET_NAME>.socket
+gunicorn settings.config.wsgi:application -b unix:/tmp/DOMAIN.socket
