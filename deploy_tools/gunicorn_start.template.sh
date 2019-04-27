bcdaea222043f11b6ae689ea905d1082e499dcef
Ubuntu <ubuntu@ip-172-31-24-77.us-west-1.compute.internal>
2019-04-27 06:13:59 +0000

    Fixing gunicorn_start template

--- deploy_tools/gunicorn_start.template.sh
+++ deploy_tools/gunicorn_start.template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu/sites/DOMAIN
-. /home/ubuntu/sites/DOMAIN/bin/activate
+. /home/ubuntu/sites/DOMAIN/virtualenv/bin/activate
 python3.6 manage.py collectstatic --noinput
-gunicorn settings.config.wsgi:application -b unix:/tmp/DOMAIN.socket
+gunicorn superlists.wsgi:application -b unix:/tmp/DOMAIN.socket
